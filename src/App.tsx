0f2477279c9c01319f412e44cc6d38aa96ee73d4
arminv <arminv@users.noreply.github.com>
2023-03-24 03:59:23 -0400

    Fix camera fov

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import Scene from './components/Scene';
 
 const App = () => {
   return (
-    <Canvas shadows camera={{ position: [14, 12, 25], fov: 40 }}>
+    <Canvas shadows camera={{ position: [14, 12, 25], fov: 50 }}>
+      {/* <axesHelper scale={30} /> */}
       <color attach='background' args={['#000000']} />
       <Suspense fallback={<Loader />}>
         <Scene />

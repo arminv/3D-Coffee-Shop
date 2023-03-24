6b3456012e30551ba500dcf67d440a9eb301c6b5
arminv <arminv@users.noreply.github.com>
2023-03-24 03:52:31 -0400

    Lighting & camera adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { Canvas } from '@react-three/fiber';
 import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
 import Loader from './components/Loader';
 import Scene from './components/Scene';
 
 const App = () => {
   return (
-    <Canvas shadows camera={{ position: [14, 9, 25], fov: 50 }}>
+    <Canvas shadows camera={{ position: [14, 12, 25], fov: 40 }}>
+      <color attach='background' args={['#000000']} />
       <Suspense fallback={<Loader />}>
         <Scene />
       </Suspense>
--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,6 +1,12 @@
 import { useRef, Ref } from 'react';
 import { PointLight, PointLightHelper } from 'three';
-import { OrbitControls, SpotLight, Stars, useHelper } from '@react-three/drei';
+import {
+  OrbitControls,
+  PresentationControls,
+  SpotLight,
+  Stars,
+  useHelper,
+} from '@react-three/drei';
 import CoffeeShop from './CoffeeShop';
 
 const Scene = () => {
@@ -9,6 +15,21 @@ const Scene = () => {
 
   return (
     <>
+      {/* Controls */}
+      <OrbitControls />
+
+      {/* Objects */}
+      <CoffeeShop />
+      {/* <PresentationControls
+        snap
+        global
+        zoom={0.9}
+        rotation={[0, Math.PI * 2, 0]}
+        polar={[0, Math.PI]}
+        azimuth={[-Math.PI / 4, Math.PI / 4]}
+      >
+        <CoffeeShop />
+      </PresentationControls> */}
       <Stars
         radius={120}
         depth={50}
@@ -18,15 +39,17 @@ const Scene = () => {
         fade
         speed={2}
       />
-      <OrbitControls />
-      <CoffeeShop />
+
+      {/* Lights */}
+      {/* <ambientLight /> */}
       <SpotLight
         // target={target}
         castShadow
-        position={[9, 14, -8]}
+        color='#f0ebc5'
+        position={[9, 15, -8]}
         penumbra={0.1}
         radiusTop={0.4}
-        radiusBottom={40}
+        radiusBottom={50}
         distance={120}
         angle={0.45}
         attenuation={20}
@@ -36,7 +59,7 @@ const Scene = () => {
       />
       <pointLight
         ref={pointLight}
-        color='whitesmoke'
+        color='#f0ebc5'
         // position={[-3, 7, 3]}
         position={[-5, 6, 5]}
         intensity={2.5}
@@ -55,7 +78,6 @@ const Scene = () => {
         shadow-mapSize-width={1024}
         shadow-mapSize-height={1024}
       /> */}
-      <ambientLight />
     </>
   );
 };
